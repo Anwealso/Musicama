f1ee1b7d79d1922df8d2c7f783606b03e105bdd6
Byron Ho <byronho24@gmail.com>
2019-08-31 13:56:02 +1000

    Added custom model Musicama to save musicama web device id

---
+++ models.py
@@ -0,0 +1,19 @@
+import spotipy
+
+DEVICE_NAME = "Musicama"
+
+class Musicama(spotipy.Spotify):
+    
+    @staticmethod
+    def get_device(sp: spotipy.Spotify):
+        # print(sp.devices())
+        # TODO: need to guess the device id of the web player?
+        devices = sp.devices()['devices']
+        for device in devices:
+            if device['name'] == 'Web Playback SDK Quick Start Player':
+            # if device['name'] == 'ONEPLUS A5010':
+                return device['id']
+
+    def __init__(self, *args, **kwargs):
+        super.__init__(*args, **kwargs)
+        self.device_id = Musicama.get_device(self)
