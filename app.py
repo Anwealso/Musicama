e4701f50d75c5bf00c9d9a9ffebf5d8118ca5e40
Byron Ho <byronho24@gmail.com>
2019-08-31 18:01:54 +1000

    App can now play music straight form the browser, no terminal interaction required

---
+++ app.py
@@ -0,0 +1,31 @@
+import flask
+import control_playback
+import authenticate
+import time
+
+app = flask.Flask(__name__)
+username = 'byronho24'
+token = None
+session = None
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/ajax/get_token', methods=['POST'])
+def get_token():
+    global token
+    token = authenticate.get_token(username)
+    return token
+
+@app.route('/ajax/start_playback', methods=['GET', 'POST'])
+def start_playback():
+    global session
+    if session is None:
+        session = authenticate.create_user(token)
+    control_playback.start(session)
+    return ""
+
+@app.route('/ajax/handle_feedback/like', methods=['POST'])
+def handle_like():
+    control_playback.handle_feedback(control_playback.LIKE, session)
